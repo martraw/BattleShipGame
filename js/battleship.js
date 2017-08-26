cab9249eacc8532739096014d4afc926f0b3ee67
martraw <trawinski1@wp.pl>
2017-08-26 18:34:42 +0200

    Add view object

--- js/battleship.js
+++ js/battleship.js
@@ -1,1 +1,18 @@
-console.log("Działa!");
+var view = {
+  displayMessage: function (msg) {
+    var messageArea = document.getElementById('messageArea');
+    console.log(messageArea);
+    messageArea.innerHTML = msg;
+  },
+
+  displayHit: function (location) {
+    var cell = document.getElementById(location);
+    console.log(cell);
+    cell.setAttribute('class', 'hit');
+  },
+
+  displayMiss: function (location) {
+    var cell = document.getElementById(location);
+    cell.setAttribute('class', 'miss');
+  },
+};

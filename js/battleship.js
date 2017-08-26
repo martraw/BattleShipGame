8064abb62b054b629c6fe2092cc35e6a9e130a05
martraw <trawinski1@wp.pl>
2017-08-26 18:35:17 +0200

    Add model object

--- js/battleship.js
+++ js/battleship.js
@@ -16,3 +16,29 @@ var view = {
     cell.setAttribute('class', 'miss');
   },
 };
+
+var model = {
+  boardSize: 7,
+  numShips: 3,
+  shipLength: 3,
+  shipsSunk: 0,
+
+  ships: [
+    { locations: ['06', '16', '26'], hits: ['', '', ''] },
+    { locations: ['24', '34', '44'], hits: ['', '', ''] },
+    { locations: ['10', '11', '12'], hits: ['', '', ''] },
+  ],
+
+  fire: function (guess) {
+    for (var i = 0; i < this.numShips.length; i++) {
+      var ship = this.ships[i];
+      var locations = ship.locations;
+      var index = locations.indexOf(guess);
+      if (index >= 0) {
+        ship.hits[index] = 'hit';
+        return true;
+      }
+      return false;
+    }
+  },
+}; //koniec model
